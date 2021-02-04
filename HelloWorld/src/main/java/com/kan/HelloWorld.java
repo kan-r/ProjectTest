6ba1034910a08ef53c6f5b99e413077b7b4d7e8e
Kan Ranganathan <krangan100@gmail.com>
2021-02-04 21:02:26 +1100

    add another printMsg

--- HelloWorld/src/main/java/com/kan/HelloWorld.java
+++ HelloWorld/src/main/java/com/kan/HelloWorld.java
@@ -3,6 +3,7 @@ package com.kan;
 public class HelloWorld {
     public static void main(String[] args) {
         printMsg("Hello World!");
+        printMsg("Hello Kan!");
     }
 
     private static void printMsg(String msg) {
