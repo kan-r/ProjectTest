39095c51130da42418ca3e2b74d7a5cc71eeb93a
Kan Ranganathan <krangan100@gmail.com>
2021-02-05 16:08:05 +1100

    Introduce constants for messages

--- HelloWorld/src/main/java/com/kan/HelloWorld.java
+++ HelloWorld/src/main/java/com/kan/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.kan;
 
 public class HelloWorld {
+
+    public static final String HELLO_KAN = "Hello Kan Ranganathan!";
+    public static final String WELCOME = "Welcome";
+
     public static void main(String[] args) {
-        printMsg("Hello Kan Ranganathan!");
-        printMsg("How are you?");
+        printMsg(HELLO_KAN);
+        printMsg(WELCOME);
     }
 
     private static void printMsg(String msg) {
